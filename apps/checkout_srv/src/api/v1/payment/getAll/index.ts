f0a31a42fbea1c22ac839c503ac08e51b4eab0fe
Viktor Pyatakov <pyatakov.viktor@gmail.com>
2022-06-25 20:38:44 +0300

    checkout_srv

---
+++ apps/checkout_srv/src/api/v1/payment/getAll/index.ts
@@ -0,0 +1,32 @@
+
+import { Route, Result, Controller } from '@library/app';
+
+
+@Route('get', '/api/v1/payments')
+class ImageController extends Controller {
+  async send(): Promise<any> {
+    const where = {};
+    const query = super.query;
+    const db = super.plugin.get('db');
+
+    const Payment = db.models['Payment'];
+
+    if ('isUse' in query) {
+      where['isUse'] = query['isUse'] === 'true';
+    }
+
+    const result = await Payment.findAll({
+      where,
+      order: [
+        ['order', 'desc'],
+      ],
+      attributes: ['code', 'displayName', 'description', 'isUse'],
+    });
+
+    return new Result()
+      .data(result.map((item) => item.toJSON()))
+      .build();
+  }
+}
+
+export default ImageController;
---
+++ apps/checkout_srv/src/index.ts
@@ -0,0 +1,24 @@
+
+import DbPlugin from "@plugin/db";
+import { Config, Application, Router } from '@library/app';
+
+
+(async function() {
+
+  const app = new Application(new Config({
+    port: Number(process.env['PORT']),
+  }));
+
+  app.addRouter(new Router([
+    import('./api/v1/delivery/getAll'),
+
+    import('./api/v1/payment/getAll'),
+  ]));
+
+  app.addPlugin('db', new DbPlugin({
+    host: process.env['DB_CONNECTION_HOST'],
+    modelsPath: 'db/models',
+  }));
+
+  await app.start();
+})();
